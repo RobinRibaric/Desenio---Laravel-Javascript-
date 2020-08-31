62c8eeab01488b35eb04ce8157b1cbee42d08e13
Robin Ribaric <robin.ribaric@appliedtechnology.se>
2020-08-31 15:04:40 +0200

    heroku

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- For Heroku -->
-    <script src="/js/app.js"></script>
+    <script src="/js/app.js" defer></script>
     <link rel="stylesheet" href="/css/app.css">
 
     <!-- Uncomment this to use in local enviornment, comment above code -->
