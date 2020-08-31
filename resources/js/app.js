57961b1faa8a3af62e48a0a57d7caaf6d531c3b1
Robin Ribaric <robin.ribaric@appliedtechnology.se>
2020-08-31 15:02:20 +0200

    heroku

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,15 +2,18 @@ import axios from 'axios';
 
 const button = document.querySelector('.button');
 
-button.addEventListener('click', () => {
+window.onload = function () {
+    button.addEventListener('click', () => {
 
-    button.innerHTML = 'loading...';
+        button.innerHTML = 'loading...';
 
-    axios.get('/getCountry')
-        .then(({ data }) => {
-            button.innerHTML = data;
-        })
-        .catch(() => {
-            button.innerHTML = 'Failed to get location!';
-        })
-})
+        axios.get('/getCountry')
+            .then(({ data }) => {
+                button.innerHTML = data;
+            })
+            .catch(() => {
+                button.innerHTML = 'Failed to get location!';
+            })
+    })
+}
+
