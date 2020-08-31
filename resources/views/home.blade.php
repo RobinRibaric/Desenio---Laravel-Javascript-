2f13dfe4f3414614e19f89ac9b3936866f3fb0f3
Robin Ribaric <robin.ribaric@appliedtechnology.se>
2020-08-31 14:47:17 +0200

    heroku

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <!-- For Heroku -->
+    <script src="/js/app.js"></script>
+    <link rel="stylesheet" href="/css/app.css">
+
+    <!-- Uncomment this to use in local enviornment, comment above code -->
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"> -->
     <title>Desenio</title>
 </head>
 
